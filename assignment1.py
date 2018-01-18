92964396637f43694d9d2b1443535dab52c19390
Nicholas A Biegel <biegelni@cams>
2018-01-18 10:07:34 -0600

    Submission of assignment 1.

---
+++ assignment1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#Needed to select a random element from list
+import random
+
+#Initialize and fill list
+alist = []
+alist.append(23453)
+alist.append("A String")
+alist.append(2341.674)
+alist.append('c')
+
+#Initialize and fill map
+amap = {}
+amap['me'] = "Nicholas Biegel"
+amap['you'] = "Michael Lewis"
+
+#Print out list, map, and others.
+print(alist)
+print(random.choice(alist))
+print(amap)
+print(amap['me'])
